fe42b5dc9ce379f3063ae7ac40b2c3f6d710aa75
jantonio2 <jesusrochavedia@gmail.com>
2022-08-24 18:01:07 -0400

    Utilizando Slivers en en el details screen

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -3,11 +3,51 @@ import 'package:flutter/material.dart';
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
+
+    // TODO: Cambiar luego por una instancia de movie
+    final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
     return const Scaffold(
-      body: Center(
-        child: Text('Details Screen'),
+      body: CustomScrollView(
+        // physics: BouncingScrollPhysics(),
+        slivers: [
+          _CustomAppBar()
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: const Text(
+            'movie.title',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover
+        ),
       ),
     );
   }
