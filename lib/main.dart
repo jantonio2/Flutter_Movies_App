b3b16678926e0705f85fbdca425553426c92ff1f
jantonio2 <jesusrochavedia@gmail.com>
2022-08-28 14:30:21 -0400

    Provider y http instalado, movies_provider creado

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/providers/movies_provider.dart';
 import 'package:peliculas/screens/screens.dart';
+import 'package:provider/provider.dart';
 
 
-void main() => runApp(const MyApp());
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: ( _ ) => MoviesProvider(), lazy: false,),
+      ],
+      child: const MyApp(),
+    );
+  }
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -11,7 +28,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Peliculas',
+      title: 'Películas',
       initialRoute: 'home',
       routes: {
         'home': ( _ ) => const HomeScreen(),
