d5a3398aeba1f421d4c287e7af8ff76726ebc127
jantonio2 <jesusrochavedia@gmail.com>
2022-08-28 16:27:30 -0400

    Instalando http

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,7 +1,12 @@
-
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier{
+
+  String _baseUrl = '';
+  String _apiKey = 'api.themoviedb.org';
+  String _language = 'es-ES';
+
   MoviesProvider() {
     print('MoviesProvider inicializado');
 
@@ -9,6 +14,6 @@ class MoviesProvider extends ChangeNotifier{
   }
 
   getOnDisplayMovies() async{
-    print('getOnDisplayMovies');
+    var url = Uri.https('', '');
   }
 }
